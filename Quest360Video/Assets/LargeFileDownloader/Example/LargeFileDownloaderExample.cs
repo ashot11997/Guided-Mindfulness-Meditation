6121a094e3af168226db6c126490ef8dcc29ce6f
Ashot Danielyan <ashot111997@gmail.com>
2020-07-06 14:41:45 +0400

    working on downloading and streaing app

---
+++ Quest360Video/Assets/LargeFileDownloader/Example/LargeFileDownloaderExample.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// import large file downloader
+using LargeFileDownloader;
+
+public class LargeFileDownloaderExample : MonoBehaviour {
+
+	const string FILE_URL_MP4 = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_30mb.mp4";
+	const string FILE_URL_FLV = "http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_30mb.flv";
+
+	FileDownloader downloader;
+
+	DownloadEvent evt = new DownloadEvent();
+
+    private string[] VideoURLs;
+
+	// Use this for initialization
+	void Start () {
+
+        VideoURLs = new string[10] { "http://Biohack.network/1.mp4", "http://Biohack.network/2.mp4", "http://biohack.network/3.mp4", "http://Biohack.network/4.mp4", "http://Biohack.network/5.mp4", "http://Biohack.network/6.mp4", "http://Biohack.network/7.mp4", "http://Biohack.network/8.mp4", "http://Biohack.network/9.mp4", "http://Biohack.network/10.mp4" };
+
+		// create downloader instance
+		downloader = new FileDownloader ();
+
+		// add events listners
+		FileDownloader.onComplete += OnDownloadComplete;
+		FileDownloader.onProgress += OnProgress;
+
+		Debug.Log (Application.persistentDataPath);
+	}
+
+
+	void OnDownloadComplete(DownloadEvent e)
+	{
+		evt = e;
+
+		if (evt.error != null)
+			Debug.Log (evt.error);
+	}
+
+	void OnProgress(DownloadEvent e)
+	{
+		evt = e;
+	}
+
+
+    /*void OnGUI()
+	{
+		if(GUILayout.Button("Download MP4"))// && !downloader.IsInQueue(FILE_URL_MP4)
+		{
+			// start downloading
+			string pathToSave = System.IO.Path.Combine (Application.persistentDataPath, System.IO.Path.GetFileName(FILE_URL_MP4));
+			downloader.Download (FILE_URL_MP4, pathToSave);
+		}
+
+		if(GUILayout.Button("Download FLV"))//&& !downloader.IsInQueue(FILE_URL_FLV)
+		{
+			// start downloading
+			string pathToSave = System.IO.Path.Combine (Application.persistentDataPath, System.IO.Path.GetFileName(FILE_URL_FLV));
+			downloader.Download (FILE_URL_FLV, pathToSave);
+		}
+
+		if (GUILayout.Button ("Download in queue MP4") && !downloader.IsInQueue(FILE_URL_MP4)) {
+			string pathToSave = System.IO.Path.Combine (Application.persistentDataPath, System.IO.Path.GetFileName(FILE_URL_MP4));
+			downloader.DownloadInQueue(FILE_URL_MP4, pathToSave);
+		}
+
+		if (GUILayout.Button ("Download in queue FLV") && !downloader.IsInQueue(FILE_URL_FLV)) {
+			string pathToSave = System.IO.Path.Combine (Application.persistentDataPath, System.IO.Path.GetFileName(FILE_URL_FLV));
+			downloader.DownloadInQueue(FILE_URL_FLV, pathToSave);
+		}
+
+		if (GUILayout.Button ("Cancel")) {
+			downloader.Cancel();
+		}
+
+		// status
+		GUILayout.Label("Total Bytes : " +evt.totalBytes);
+		GUILayout.Label("Downloaded Bytes : " +evt.downloadedBytes);
+		GUILayout.Label("Downloading Progress (%): " +evt.progress);
+		GUILayout.Label ("\nStatus : " + evt.status);
+		GUILayout.Label ("\nError : " + ((!string.IsNullOrEmpty(evt.error)) ? evt.error : ""));
+
+	}*/
+
+
+    private bool IsStartedDownloading = false;
+    private Text CurrentPercentageText;
+    private Image CurrentFillingBar;
+
+    private VideoThumbnailContainer CurrentVideoThumbnail;
+
+    public void DownloadVideo(int id, Text percentageText, Image Fillingbar, GameObject LoadingBar, VideoThumbnailContainer VideoThumbnail) {
+        int newId = id - 1;
+        CurrentPercentageText = percentageText;
+        CurrentFillingBar = Fillingbar;
+        LoadingBar.SetActive(true);
+        CurrentVideoThumbnail = VideoThumbnail;
+
+        IsStartedDownloading = true;
+        string pathToSave = System.IO.Path.Combine(Application.persistentDataPath, System.IO.Path.GetFileName(VideoURLs[newId]));
+        downloader.Download(VideoURLs[newId], pathToSave);
+    }
+
+    private void Update()
+    {
+        if (IsStartedDownloading)
+        {
+            CurrentPercentageText.text = evt.progress.ToString() + " %";
+            CurrentFillingBar.fillAmount += (evt.progress / 100);
+
+            Debug.LogError(evt.progress);
+
+            if (evt.progress == 100)
+            {
+                CurrentVideoThumbnail.VideoDownloaded();
+                IsStartedDownloading = false;
+            }
+        }
+    }
+}
---
+++ Quest360Video/Assets/Scripts/VideoThumbnailContainer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoThumbnailContainer : MonoBehaviour
+{
+    public Button DownloadBtn;
+    public Button StreamBtn;
+
+    private int ID;
+    private bool isDownloaded;
+
+    public Text percentageText;
+
+    public GameObject LoadingBar;
+
+    public Image FillingBar;
+
+    private LargeFileDownloaderExample VideoDownloader;
+
+    void Awake()
+    {
+        DownloadBtn.onClick.AddListener(DownloadVideo);
+    }
+
+    void Start()
+    {
+        Text text = GetComponentInChildren<Text>();
+        if (text != null)
+        {
+            string[] newText = text.text.Split(' ');
+            int id = int.Parse(newText[1]);
+            Setup(id);
+        }
+
+        GameObject downloader = GameObject.FindGameObjectWithTag("VideoDownloader");
+        if (downloader != null)
+        {
+            VideoDownloader = downloader.GetComponent<LargeFileDownloaderExample>();
+        }
+
+        LoadingBar.SetActive(false);
+        DownloadBtn.gameObject.SetActive(true);
+
+        /*if (PlayerPrefs.GetInt("downloaded") == 0 || PlayerPrefs.GetInt("downloaded") == null)
+        {
+            StreamBtn.interactable = false;
+            DownloadBtn.interactable = true;
+        }
+        else
+        {
+            StreamBtn.interactable = true;
+            DownloadBtn.interactable = false;
+        }*/
+    }
+
+    public void Setup(int id) {
+        ID = id;
+    }
+
+    public void Downloaded() {
+        StreamBtn.interactable = true;
+        DownloadBtn.interactable = false;
+    }
+
+    void DownloadVideo() {
+        Debug.Log("Starting " + ID + " Video");
+        VideoDownloader.DownloadVideo(ID, percentageText, FillingBar, LoadingBar, this);
+    }
+
+    public void VideoDownloaded() {
+        isDownloaded = true;
+        Debug.LogError("Video Successfully Downloaded");
+    }
+
+}
