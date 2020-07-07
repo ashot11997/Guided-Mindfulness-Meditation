07132664a7364a722d5df9565e194b70c31c71cd
Ashot Danielyan <ashot111997@gmail.com>
2020-07-07 18:45:34 +0400

    finished randoming musics

--- Quest360Video/Assets/Scripts/VideoEngine.cs
+++ Quest360Video/Assets/Scripts/VideoEngine.cs
@@ -2,12 +2,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
 
 public class VideoEngine : MonoBehaviour
 {
     public MediaPlayer VideoPlayer;
 
+    public string MenuName;
+    
     public Button BackButton;
+
+    public AudioSource Audio_Voice;
+    public AudioSource Audio_Music;
 
     private void Awake()
     {
@@ -16,11 +23,50 @@ public class VideoEngine : MonoBehaviour
 
     private void Start()
     {
-        string url = System.IO.Path.Combine(Application.persistentDataPath, System.IO.Path.GetFileName(PlayerPrefs.GetInt("ID")+ ".mp4"));
-        VideoPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, url);
+        if (Audio_Voice == null)
+        {
+            // this is for first project
+            string url = System.IO.Path.Combine(Application.persistentDataPath, System.IO.Path.GetFileName(PlayerPrefs.GetInt("ID") + ".mp4"));
+            VideoPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, url);
+        }
+        else
+        {
+            // this is for second project
+            string url = PlayerPrefs.GetString("VideoUrl");
+            VideoPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, url);
+            StartCoroutine(PickUpRandomVoice(PlayerPrefs.GetString("VoiceUrl")));
+            StartCoroutine(PickUpRandomMusic(PlayerPrefs.GetString("MusicUrl")));
+        }
+        
     }
 
     void GoToMenu() {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene(MenuName);
+    }
+
+    private IEnumerator PickUpRandomVoice(string url) {
+        WWW www = new WWW(url);
+        yield return www;
+
+        AudioClip audio = www.GetAudioClip(false,true, AudioType.MPEG);
+        if (audio == null || audio.loadState == AudioDataLoadState.Unloaded)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Audio_Voice.clip = audio;
+        Audio_Voice.Play();
+    }
+    private IEnumerator PickUpRandomMusic(string url)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        AudioClip audio = www.GetAudioClip(false, true, AudioType.MPEG);
+        if (audio == null || audio.loadState == AudioDataLoadState.Unloaded)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Audio_Music.clip = audio;
+        Audio_Music.Play();
     }
 }
